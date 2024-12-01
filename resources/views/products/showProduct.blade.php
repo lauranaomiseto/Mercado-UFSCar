e59300575a3bece8fc2d360c96c89fa35fd28644
Laura Naomi Seto <lauranaomiseto@gmail.com>
2024-12-01 13:19:05 -0300

    [#US01] tela de visualização detalhada de produtos

--- resources/views/products/showProduct.blade.php
+++ resources/views/products/showProduct.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
     
-<div class="max-w-[850px] w-4/5 m-auto my-[50px] border-b-2 border-orange">
+    <div class="max-w-[850px] w-4/5 m-auto my-[50px] border-b-2 border-orange">
         <x-buttons.icon route="{{ route('products') }}" text="Voltar">
         </x-buttons.icon>
     
         <div class="flex justify-between content-center flex-wrap my-[50px] gap-5">
-            <h1 class="text-4xl font-light"> 
+            <h1 class="text-4xl font-light w-4/5 overflow-hidden whitespace-nowrap text-ellipsis"> 
                 {{ $product->descricao }}
             </h1>
         
@@ -19,6 +19,18 @@
 
             </form>
         </div>
+    </div>
+
+    <div class="max-w-[850px] w-4/5 m-auto">
+        <p class="mb-[15px]">
+            <strong>Código:</strong> #{{ $product->id }}
+        </p>
+        <p class="mb-[15px]">
+            <strong>Descrição completa:</strong> {{ $product->descricao }}
+        </p>
+        <p class="mb-[15px]">
+            <strong>Preço:</strong> R${{ number_format($product->preco, 2, ',', '') }}
+        </p>
     </div>
 
 </x-layout>
