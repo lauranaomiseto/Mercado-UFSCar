ad72b765b163c56507852190047bdc66846b716f
Laura Naomi Seto <lauranaomiseto@gmail.com>
2024-12-01 16:45:18 -0300

    [#US01] atualização do tipo da coluna id

--- database/migrations/2024_12_01_160711_produto.php
+++ database/migrations/2024_12_01_160711_produto.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('produto', function (Blueprint $table){
-            $table->id();
+            $table->increments('id');
             $table->string('descricao'); // por default length 255
             $table->float('preco');
             $table->timestamps();
