5bf1895b3ab63ad9dcf7706ebb8e860b789db640
Badast <jeanrodriguesrocha@gmail.com>
2024-12-16 20:01:50 -0300

    [#US03] adição das tabelas venda e prod_venda ao migrations

---
+++ database/migrations/2024_12_16_167121_prod_venda.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+		
+		Schema::dropIfExists('prod_venda');
+		
+        Schema::create('prod_venda', function (Blueprint $table){
+            $table->unsignedInteger('id_produto');
+            $table->unsignedInteger('id_lote');
+			$table->unsignedInteger('id_venda');
+            $table->unsignedInteger('quantidade');
+			$table->primary(['id_produto', 'id_lote', 'id_venda']);
+            $table->foreign('id_produto')->references('id')->on('produto');
+			$table->foreign('id_lote')->references('id')->on('lote');
+			$table->foreign('id_venda')->references('id')->on('venda');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prod_venda');
+    }
+};
