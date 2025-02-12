86360c97f41f889c873918657b2f3d25740db7aa
Badast <jeanrodriguesrocha@gmail.com>
2025-02-11 21:37:08 -0300

    [#US02] mudança no migrate do lote para aceitar valores nulos

--- database/migrations/2024_12_01_162927_lote.php
+++ database/migrations/2024_12_01_162927_lote.php
@@ -10,16 +10,20 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('lote', function (Blueprint $table){
-            $table->increments('id');
-            $table->unsignedInteger('id_produto');
-            $table->unsignedInteger('quantidade');
-            $table->date('validade');
-            $table->timestamps();
-            $table->foreign('id_produto')->references('id')->on('produto');
-        });
-    }
+	{
+		Schema::create('lote', function (Blueprint $table) {
+			$table->increments('id');
+			$table->unsignedInteger('id_produto')->nullable();
+			$table->unsignedInteger('quantidade');
+			$table->date('validade');
+			$table->timestamps();
+
+			$table->foreign('id_produto')
+				  ->references('id')
+				  ->on('produto')
+				  ->onDelete('set null');
+		});
+	}
 
     /**
      * Reverse the migrations.
